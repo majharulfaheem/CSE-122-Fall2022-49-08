c9c3eb6323880873919a93e2a8f136c40197d721
Md. Majharul Kamal <90193627+majharulfaheem@users.noreply.github.com>
2022-08-16 22:24:49 +0600

    Create 3_ 543_49-08.cpp

---
+++ lab-2/3_ 543_49-08.cpp
@@ -0,0 +1,17 @@
+// codeforces problem no- 50A
+// problem name- domino piling
+
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int m,n,s;
+    cin>>m>>n;
+    if(m>=1 && n<=16)
+    {
+        s=(m*n)/2;
+    }
+    cout<<s<<endl;
+
+}
