3e40258075387248247d91baa307787e680d511d
Md. Majharul Kamal <90193627+majharulfaheem@users.noreply.github.com>
2022-08-16 22:06:28 +0600

    Create 1_ 543_49-08.cpp

---
+++ Lab-1/1_ 543_49-08.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    if(n%2==0 && n!=2)
+        cout<<"yes"<<endl;
+    else
+        cout<<"no"<<endl;
+
+    return 0;
+}
